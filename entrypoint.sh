2bc1c58611b7d688e53f96388417c58150132160
gritzman <gilles.ritzmann@unil.ch>
2021-06-29 16:41:26 +0200

    fix init_db()

--- entrypoint.sh
+++ entrypoint.sh
@@ -210,7 +210,7 @@ initialize_system() {
 
 init_db() {
   echo "Initializing Cachet database ..."
-  php artisan cachet:install --no-interaction
+  php artisan app:install --no-interaction || php artisan cachet:install --no-interaction
   check_configured
 }
 
@@ -234,6 +234,10 @@ start_system() {
   seed_db
   echo "Starting Cachet! ..."
   php artisan config:cache
+  # BEGIN of the hack to fix PHP 7.2, as suggested in: https://github.com/CachetHQ/Cachet/issues/4132
+  php artisan route:cache
+  echo "if(version_compare(PHP_VERSION, '7.2.0', '>=')) { error_reporting(E_ALL ^ E_NOTICE ^ E_WARNING); }" >> bootstrap/cache/routes.php
+  # END of the hack
   /usr/bin/supervisord -n -c /etc/supervisor/supervisord.conf
 }
 
